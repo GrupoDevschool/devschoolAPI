91fec776be62a0ed349e06f7fbd9fb0abd855306
Grupo Devschool <89432246+GrupoDevschool@users.noreply.github.com>
2021-09-04 15:11:49 -0300

    Add files via upload

---
+++ src/main/java/br/com/devschool/devschool/DevschoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.devschool.devschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevschoolApplication.class, args);
+	}
+
+}
