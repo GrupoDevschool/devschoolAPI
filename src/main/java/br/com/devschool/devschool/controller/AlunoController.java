f1e97f85c41bf2676a3f812bfc2ed8a00f44766a
Lucas Cezario <lucasdsc96@gmail.com>
2021-09-15 23:31:24 -0300

    fix aluno

--- src/main/java/br/com/devschool/devschool/controller/AlunoController.java
+++ src/main/java/br/com/devschool/devschool/controller/AlunoController.java
@@ -4,15 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.devschool.devschool.model.Aluno;
 import br.com.devschool.devschool.model.dto.AlunoDTO;
@@ -33,9 +25,9 @@ public class AlunoController {
         return ResponseEntity.ok(AlunoDTO.converter(alunos));
     }
 
-    //  get: Listar 1 colaborador pelo cpf
-    @GetMapping("/{matricula}")
-    public ResponseEntity<AlunoDTO> listarAlunoByMatricula(@PathVariable Integer matricula) {
+
+    @GetMapping("/{matricula}" )
+    public ResponseEntity<AlunoDTO> listarAlunoByMatricula(@PathVariable Integer matricula ) {
         AlunoDTO alunoDTO = new AlunoDTO(alunoService.listarAlunoByMatricula(matricula));
         return ResponseEntity.ok(alunoDTO);
     }
