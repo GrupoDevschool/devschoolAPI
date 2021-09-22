8a4c5550035a765aa2d90fe34e9e9bc174109070
Brunno98 <brunnosantana98@gmail.com>
2021-09-22 11:18:03 -0300

    Criacao do ErrorHandler

---
+++ src/main/java/br/com/devschool/devschool/controller/ErrorHandler.java
@@ -0,0 +1,48 @@
+package br.com.devschool.devschool.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.devschool.devschool.infrastructure.exception.ContentNotFoundException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+	@ExceptionHandler(ContentNotFoundException.class)
+	public ResponseEntity<Map<String, String>> conteudoNaoEncontrado(ContentNotFoundException exception) {
+		Map<String, String> response = new HashMap<>();
+		response.put("menssagem", exception.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> atributoInvalido(MethodArgumentNotValidException exception) {
+		Map<String, String> errors = new HashMap<>();
+	    exception.getBindingResult().getAllErrors().forEach((error) -> {
+	        String fieldName = ((FieldError) error).getField();
+	        String errorMessage = error.getDefaultMessage();
+	        errors.put(fieldName, errorMessage);
+	    });
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+	}
+	
+	public ResponseEntity<Map<String, String>> exclusaoDeConteudoComDependencia() {
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<Map<String, String>> tipoDoAtributoIncorreto(HttpMessageNotReadableException exception) {
+		Map<String, String> response = new HashMap<>();
+		response.put("menssagem", exception.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	}
+	
+}
